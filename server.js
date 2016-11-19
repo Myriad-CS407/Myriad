fe0f8daec3d7a5f3a64af08cd28d8f69fb7548b8
Cody Henderson <cody@cthenderson.com>
2016-11-19 11:50:35 -0600

    Add signin page

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express')
   , myriad = require('jade').compileFile(__dirname + '/source/templates/index.jade')
   , news_sources = require('jade').compileFile(__dirname + '/source/templates/news_sources.jade')
   , settings = require('jade').compileFile(__dirname + '/source/templates/managesources.jade')
+  , signin = require('jade').compileFile(__dirname + '/source/templates/signin.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -27,22 +28,22 @@ app.get('/myriad/', function(req, res, next) {
   }
 })
 
-app.get('/news_sources', function(req, res, next) {
+app.get('/settings', function(req, res, next) {
   try {
-    var html = news_sources( {title: 'Myriad'})
+    var html = settings( {title: 'Myriad - Settings'})
     res.send(html)
   } catch (e) {
     next(e)
   }
 })
 
-app.get('/settings', function(req, res, next) {
-  try {
-    var html = settings( {title: 'Myriad'})
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+app.get('/signin', function(req, res, next) {
+    try {
+        var html = signin( {title: 'Myriad - Sign In'})
+        res.send(html)
+    } catch (e) {
+        next(e)
+    }
 })
 
 app.listen(process.env.PORT || 5000, function () {
